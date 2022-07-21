e8a0ba1d46150b0b6a43482ef1a8fb997ddf0250
si-shakil <sishakil850@gmail.com>
2022-07-21 11:06:23 +0600

    controls button implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
+import { useState } from "react";
+
 function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [error, setError] = useState(null);
+  // to format json
+  const formatJSON = () => {
+    try {
+      const formatted = JSON.stringify(JSON.parse(input), null, 4);
+      setError(null);
+      setOutput(formatted);
+    } catch (err) {
+      setError(err);
+      setOutput(err.message);
+    }
+  };
+  // to minify JSON
+  const minifyFormat = () => {
+    try {
+      const minified = JSON.stringify(JSON.parse(input));
+      setError(null);
+      setOutput(minified);
+    } catch (err) {
+      setError(err);
+      setOutput(err.message);
+    }
+  };
+  // to clear both textarea
+  const clearAll = () => {
+    setInput("");
+    setOutput("");
+  };
   return (
     <div className="h-[100vh] flex items-center flex-col justify-center bg-[#f2efef]">
       <div className="flex items-center w-[90%] box-border">
-        {/* First Textarea. textarea style is implemented from index.css file  */}
+        {/* First Textarea. textarea style is  implemented from index.css file  */}
         <textarea
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
           className="text-area"
-          name=""
-          id=""
+          name="input"
+          id="input"
           placeholder="Enter JSON data"
         ></textarea>
         {/* Controls button. button styles are implemented from index.css file */}
         <div className="flex flex-col flex-[0.2] mx-10">
-          <button type="button" className="control-button">
+          <button onClick={formatJSON} type="button" className="control-button">
             Format
           </button>
-          <button type="button" className="control-button">
+          <button
+            onClick={minifyFormat}
+            type="button"
+            className="control-button"
+          >
             Minify
           </button>
           <button
+            onClick={clearAll}
             type="button"
             className="control-button bg-red-500 hover:bg-red-800"
           >
@@ -26,9 +65,11 @@ function App() {
         </div>
         {/* Second Textarea. textarea style is implemented from index.css file  */}
         <textarea
-          className="text-area"
-          name=""
-          id=""
+          readOnly
+          value={output}
+          className={`text-area ${error && "text-red-600"}`}
+          name="output"
+          id="output"
           placeholder="Your formatted result is here"
         ></textarea>
       </div>
