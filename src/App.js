83e51ab417cbe28b37b23f5ffa1780cc3468765b
si-shakil <sishakil850@gmail.com>
2022-07-21 09:24:21 +0600

    initial

---
+++ src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="">
+      <textarea name="" id=""></textarea>
+      <div className="controls">
+        <button type="button">Format</button>
+        <button type="button">Minify</button>
+        <button type="button">Clear</button>
+      </div>
+      <textarea name="" id=""></textarea>
+    </div>
+  );
+}
+
+export default App;
